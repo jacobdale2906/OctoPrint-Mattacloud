976d5753f751dcf9bc2f57cf567b753b56ceb796
Douglas Brion <db1415@ic.ac.uk>
2019-07-17 17:36:28 +0100

    Creating project structure

---
+++ octoprint_mattacloud/static/js/mattacloud.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Mattacloud
+ *
+ * Author: Douglas Brion
+ * License: AGPLv3
+ */
+$(function() {
+    function MattacloudViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: MattacloudViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_mattacloud, #tab_plugin_mattacloud, ...
+        elements: [ /* ... */ ]
+    });
+});
