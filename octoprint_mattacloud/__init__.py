edbe219be2b917dee19f4bc67f463c6ba5c3797c
Douglas Brion <db1415@ic.ac.uk>
2019-07-17 18:04:12 +0100

    Plugin is loading and working with OctoPrint

--- octoprint_mattacloud/__init__.py
+++ octoprint_mattacloud/__init__.py
@@ -1,72 +1,61 @@
-# coding=utf-8
-from __future__ import absolute_import
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# coding = utf - 8
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+from __future__ import absolute_import
 
 import octoprint.plugin
 
-class MattacloudPlugin(octoprint.plugin.SettingsPlugin,
+import logging
+
+from .watcher import ImageHandler
+from .ws import Socket
+
+
+class MattacloudPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.SettingsPlugin,
+                       octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.AssetPlugin,
-                       octoprint.plugin.TemplatePlugin):
+                       octoprint.plugin.SimpleApiPlugin,
+                       octoprint.plugin.EventHandlerPlugin):
 
-	##~~ SettingsPlugin mixin
+    def get_settings_defaults(self):
+        return dict()
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    def get_assets(self):
+        return dict(
+            js=['js/mattacloud.js'],
+            css=['css/mattacloud.css'],
+            less=['less/mattacloud.less']
+        )
 
-	##~~ AssetPlugin mixin
+    def get_template_configs(self):
+        self._logger.info("OctoPrint-Mattacloud - is loading template configurations.")
+        return [
+            dict(type="settings", custom_bindings=False)
+        ]
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/mattacloud.js"],
-			css=["css/mattacloud.css"],
-			less=["less/mattacloud.less"]
-		)
+    def get_update_information(self):
+        return dict(mattacloud=dict(
+            displayVersion=self._plugin_version,
+            type='github_release',
+            user='dougbrion',
+            repo='OctoPrint-Mattacloud',
+            current=self._plugin_version,
+            pip='https://github.com/dougbrion/OctoPrint-Mattacloud/archive/{target_version}.zip',
+        ))
 
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			mattacloud=dict(
-				displayName="Mattacloud Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="dougbrion",
-				repo="OctoPrint-Mattacloud",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/dougbrion/OctoPrint-Mattacloud/archive/{target_version}.zip"
-			)
-		)
+    def on_after_startup(self):
+        self._logger.info("Starting OctoPrint-Mattacloud Plugin...")
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Mattacloud Plugin"
+__plugin_name__ = 'Mattacloud'
+
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = MattacloudPlugin()
-
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+    global __plugin_implementation__
+    __plugin_implementation__ = MattacloudPlugin()
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        'octoprint.plugin.softwareupdate.check_config': __plugin_implementation__.get_update_information
+    }
